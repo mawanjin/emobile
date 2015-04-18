0c103a8a6fda2154894aaa440d93101359cca8f5
wanjinma <13774266242@hotmail.com>
2015-04-18 19:59:16 +0800

    init
    init

---
+++ js/main.js
@@ -0,0 +1,237 @@
+// 
+// Here is how to define your module 
+// has dependent on mobile-angular-ui
+// 
+
+
+var mApp = angular.module('phonecat', [
+  'ngRoute',
+  'mobile-angular-ui',
+  'phonecatControllers',
+  'phonecatServices',
+
+  // touch/drag feature: this is from 'mobile-angular-ui.gestures.js'
+  // it is at a very beginning stage, so please be careful if you like to use
+  // in production. This is intended to provide a flexible, integrated and and 
+  // easy to use alternative to other 3rd party libs like hammer.js, with the
+  // final pourpose to integrate gestures into default ui interactions like 
+  // opening sidebars, turning switches on/off ..
+  'mobile-angular-ui.gestures'
+]);
+
+mApp.config(function($routeProvider) {
+  $routeProvider.when('/phones',          {templateUrl: 'phone-list.html',controller: 'PhoneListCtrl', reloadOnSearch: false}).
+  when('/phones/:phoneId', {templateUrl: 'phone-detail.html',controller: 'PhoneDetailCtrl'}).
+  otherwise({redirectTo: '/phones'});
+});
+
+
+var app = angular.module('emobile', [
+  'ngRoute',
+  'mobile-angular-ui',
+  
+  
+  // touch/drag feature: this is from 'mobile-angular-ui.gestures.js'
+  // it is at a very beginning stage, so please be careful if you like to use
+  // in production. This is intended to provide a flexible, integrated and and 
+  // easy to use alternative to other 3rd party libs like hammer.js, with the
+  // final pourpose to integrate gestures into default ui interactions like 
+  // opening sidebars, turning switches on/off ..
+  'mobile-angular-ui.gestures'
+]);
+
+// 
+// You can configure ngRoute as always, but to take advantage of SharedState location
+// feature (i.e. close sidebar on backbutton) you should setup 'reloadOnSearch: false' 
+// in order to avoid unwanted routing.
+// 
+app.config(function($routeProvider) {
+  $routeProvider.when('/',              {templateUrl: 'home.html', controller:'HomeController', reloadOnSearch: false});
+  
+  $routeProvider.when('/toggle',        {templateUrl: 'toggle.html', reloadOnSearch: false}); 
+  $routeProvider.when('/tabs',          {templateUrl: 'tabs.html', reloadOnSearch: false}); 
+  $routeProvider.when('/accordion',     {templateUrl: 'accordion.html', reloadOnSearch: false}); 
+  $routeProvider.when('/overlay',       {templateUrl: 'overlay.html', reloadOnSearch: false}); 
+  $routeProvider.when('/forms',         {templateUrl: 'forms.html', reloadOnSearch: false});
+  $routeProvider.when('/dropdown',      {templateUrl: 'dropdown.html', reloadOnSearch: false});
+  $routeProvider.when('/drag',          {templateUrl: 'drag.html', reloadOnSearch: false});
+  $routeProvider.when('/scroll',      {templateUrl: 'scroll.html', reloadOnSearch: false});
+  
+});
+
+//
+// `$drag` example: drag to dismiss
+//
+app.directive('dragToDismiss', function($drag, $parse, $timeout){
+  return {
+    restrict: 'A',
+    compile: function(elem, attrs) {
+      var dismissFn = $parse(attrs.dragToDismiss);
+      return function(scope, elem, attrs){
+        var dismiss = false;
+
+        $drag.bind(elem, {
+          constraint: {
+            minX: 0, 
+            minY: 0, 
+            maxY: 0 
+          },
+          move: function(c) {
+            if( c.left >= c.width / 4) {
+              dismiss = true;
+              elem.addClass('dismiss');
+            } else {
+              dismiss = false;
+              elem.removeClass('dismiss');
+            }
+          },
+          cancel: function(){
+            elem.removeClass('dismiss');
+          },
+          end: function(c, undo, reset) {
+            if (dismiss) {
+              elem.addClass('dismitted');
+              $timeout(function() { 
+                scope.$apply(function() {
+                  dismissFn(scope);  
+                });
+              }, 400);
+            } else {
+              reset();
+            }
+          }
+        });
+      };
+    }
+  };
+});
+
+
+app.controller('HomeController', function($rootScope, $scope){
+  $('#owl-demo').owlCarousel({
+    items: 1,
+    autoPlay: true
+  });
+});
+
+app.controller('ActivityController', function($rootScope, $scope,$http){
+  $scope.userAgent = navigator.userAgent;
+var scrollItems = [];
+
+  for (var i=1; i<=100; i++) {
+    scrollItems.push('Item ' + i);
+  }
+
+  $scope.scrollItems = scrollItems;
+
+	$http.get('json/activity.json').
+	  success(function(data, status, headers, config) {
+    	// this callback will be called asynchronously
+	    // when the response is available
+
+	    $scope.activitys = data;
+	    
+  }).
+  error(function(data, status, headers, config) {
+    // called asynchronously if an error occurs
+    // or server returns response with an error status.
+	
+  });
+});
+
+
+//
+// For this trivial demo we have just a unique MainController 
+// for everything
+//
+app.controller('MainController', function($rootScope, $scope,$http){
+
+//$scope.phones=[{"name":"Nexus S"},{"name":"Galxy Note2"}];
+
+  // User agent displayed in home page
+  $scope.userAgent = navigator.userAgent;
+  
+  // Needed for the loading screen
+  $rootScope.$on('$routeChangeStart', function(){
+    $rootScope.loading = true;
+  });
+
+  $rootScope.$on('$routeChangeSuccess', function(){
+    $rootScope.loading = false;
+  });
+
+  // Fake text i used here and there.
+  $scope.lorem = 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Vel explicabo, aliquid eaque soluta nihil eligendi adipisci error, illum corrupti nam fuga omnis quod quaerat mollitia expedita impedit dolores ipsam. Obcaecati.';
+
+  // 
+  // 'Scroll' screen
+  // 
+  var scrollItems = [];
+
+  for (var i=1; i<=100; i++) {
+    scrollItems.push('Item ' + i);
+  }
+
+  $scope.scrollItems = scrollItems;
+
+  $scope.bottomReached = function() {
+    alert('Congrats you scrolled to the end of the list!');
+  }
+
+  // 
+  // Right Sidebar
+  // 
+  $scope.chatUsers = [
+    { name: 'Carlos  Flowers', online: true },
+    { name: 'Byron Taylor', online: true },
+    { name: 'Jana  Terry', online: true },
+    { name: 'Darryl  Stone', online: true },
+    { name: 'Fannie  Carlson', online: true },
+    { name: 'Holly Nguyen', online: true },
+    { name: 'Bill  Chavez', online: true },
+    { name: 'Veronica  Maxwell', online: true },
+    { name: 'Jessica Webster', online: true },
+    { name: 'Jackie  Barton', online: true },
+    { name: 'Crystal Drake', online: false },
+    { name: 'Milton  Dean', online: false },
+    { name: 'Joann Johnston', online: false },
+    { name: 'Cora  Vaughn', online: false },
+    { name: 'Nina  Briggs', online: false },
+    { name: 'Casey Turner', online: false },
+    { name: 'Jimmie  Wilson', online: false },
+    { name: 'Nathaniel Steele', online: false },
+    { name: 'Aubrey  Cole', online: false },
+    { name: 'Donnie  Summers', online: false },
+    { name: 'Kate  Myers', online: false },
+    { name: 'Priscilla Hawkins', online: false },
+    { name: 'Joe Barker', online: false },
+    { name: 'Lee Norman', online: false },
+    { name: 'Ebony Rice', online: false }
+  ];
+
+  //
+  // 'Forms' screen
+  //  
+  $scope.rememberMe = true;
+  $scope.email = 'me@example.com';
+  
+  $scope.login = function() {
+    alert('You submitted the login form');
+  };
+
+  // 
+  // 'Drag' screen
+  // 
+  $scope.notices = [];
+  
+  for (var j = 0; j < 10; j++) {
+    $scope.notices.push({icon: 'envelope', message: 'Notice ' + (j + 1) });
+  }
+
+  $scope.deleteNotice = function(notice) {
+    var index = $scope.notices.indexOf(notice);
+    if (index > -1) {
+      $scope.notices.splice(index, 1);
+    }
+  };
+});
