231b5ad763a83e428c56fa7cab20476368f60015
wanjinma <13774266242@hotmail.com>
2015-04-26 00:00:37 +0800

    海外之家

--- js/ctrl/abroad_ctrl.js
+++ js/ctrl/abroad_ctrl.js
@@ -29,22 +29,95 @@ app.config(function($routeProvider) {
 });
 
 app.controller('detailController', function($rootScope, $scope,$http,$routeParams,$cookieStore,$location){
-	$scope.abroadId = $routeParams.id;
 
-	$http.get('/edu/f/edu/abroad/get?id='+$routeParams.id).
+    $scope.$watch('$viewContentLoaded', function() {
+        $(".modal").hide();
+    });
+
+    $scope.closeEnrollDialog = function(){
+        $(".modal").hide();
+    };
+
+	$scope.abroadId = $routeParams.id;
+    if($cookieStore.get("login")==true){
+      $scope.user = $cookieStore.get("user");
+    }
+
+    //网络请求数据
+    var url = "/edu/f/edu/abroad/get?abroadhome="+$routeParams.id;
+    if($scope.user != undefined){
+        url = url +'&uid='+$scope.user.id;
+    }
+	$http.get(url).
 	  success(function(data, status, headers, config) {
 	    $scope.abroad = data;
-	    
-  }).
-  error(function(data, status, headers, config) {
-    // called asynchronously if an error occurs
-    // or server returns response with an error status.
-	
-  });
+        if($scope.abroad.enrolled==true){
+          $("#enrollContainer").html("已报名");
+          $("#enrollContainer").unbind();
+        }
+      }).
+        error(function(data, status, headers, config) {
+    });
 
   $scope.enroll = function(){
-    //alert($cookieStore.get("name"));
-    $location.path("/login")
+
+    if($cookieStore.get("login")==true){//进行报名操作
+      $scope.user = $cookieStore.get("user");
+
+      $http.get('/edu/f/edu/abroad/enroll/save?userId='+$scope.user.id+'&abroadId='+$scope.abroadId).
+          success(function(data, status, headers, config) {
+            $scope.enroll_result = data;
+
+            if($scope.enroll_result==true){
+              //将我要报名的文字变成已报名
+                $(".modal").show();
+              $("#enrollContainer").html("已报名");
+              $("#enrollContainer").unbind();
+            }else{
+              alert("注册失败");
+            }
+
+          }).
+          error(function(data, status, headers, config) {
+            alert("注册失败")
+          });
+
+    }else{//去登录
+      $location.path("/login")
+    }
+
+  };
+
+  $scope.login = function(){
+
+    if($scope.loginName==undefined || $scope.loginName==""){
+      alert("请输入用户名");
+      return
+    }
+
+    if($scope.password==undefined || $scope.password==""){
+      alert("请输入密码");
+      return
+    }
+
+      $http.get('/edu/f/edu/account/login?loginName='+$scope.loginName+'&password='+$scope.password).
+          success(function(data, status, headers, config) {
+            $scope.login_rs = data;
+            if($scope.login_rs.rs==true){
+              //存储用户信息
+              $cookieStore.put("login",true);
+              $cookieStore.put("user",$scope.login_rs.euser);
+              window.history.back();
+
+            }else{
+              alert("用户名或密码错误");
+              $cookieStore.put("login",false);
+            }
+
+          }).
+          error(function(data, status, headers, config) {
+            alert("登录失败")
+          });
   };
   
   $scope.deliberatelyTrustDangerousSnippet = function() {  
